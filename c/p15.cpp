c4bd396dafbcc66d8f329c7d70fe1f5ed211903e
Kishorekb2003 <132202728+Kishorekb2003@users.noreply.github.com>
2024-03-16 00:20:59 -0400

    team bugger_king p15.cpp

--- c/p15.cpp
+++ c/p15.cpp
@@ -9,13 +9,14 @@ void Upper(char *word) {
 }
 
 int Check(char *S) {
-    for(int k = 0, v = 0; S[k]; k++)
+    int v=0;
+    for(int k = 0; S[k]; k++)
         switch(S[k]) {
-            case A:
-            case E:
-            case I:
-            case O:
-            case U: v++;
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U': v++;
         }
     return v;
 }
